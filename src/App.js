6de4780690336bb4ee917299b0ec8f169f9a5807
baf62495 <baf62495@aol.com>
2019-10-18 14:55:55 -0400

    basic router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <p>This is the App component</p>
-    </div>
-  );
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+			  	<header className='app__header'>
+						<Link to='/'><h1>Noteful</h1></Link>
+				</header>
+				<main className='app__main'>
+
+				</main>
+				<sidebar className='app__sidebar'>
+
+				</sidebar>
+			</div>
+		);
+	}
 }
-
-export default App;
