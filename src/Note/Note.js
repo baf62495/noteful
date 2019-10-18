45406bdb538df9102037de0dbdcada386f27cd53
baf62495 <baf62495@aol.com>
2019-10-18 18:23:50 -0400

    Note component layout complete

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './Note.css';
+import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
 
 export default function Note(props) {
 	return (
-		<p>This is the Note Component</p>
+		<div className='note'>
+			<h2 className='Note__title'>
+				<Link to={`./note/${props.id}`}>
+					{props.name}
+				</Link>
+			</h2>
+			<div className='Note__modified'>
+				Last modified on {' '}
+				<span className='date'>
+					{format(new Date(props.modified), 'dd/MM/yyyy')}
+				</span>
+			</div>
+		</div>
 	)
 }
--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -5,6 +5,22 @@ import { Link } from 'react-router-dom';
 
 export default function NoteListMain(props) {
 	return (
-		<p>This is the NoteListMain {props.match.params.folderId} Component</p>
+		<section className='NoteListMain'>
+			<ul>
+				{props.notes.map(note =>
+					<li key={note.id}>
+						<Note
+							id={note.id}
+							name={note.name}
+							modified={note.modified}
+						/>
+					</li>
+				)}
+			</ul>
+		</section>
 	)
+}
+
+NoteListMain.defaultProps = {
+	notes: []
 }
