9c0491bc121e633969cba6b3df86d03674ffd35d
baf62495 <baf62495@aol.com>
2019-10-18 15:42:48 -0400

    layout css added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 import { Route, Link } from 'react-router-dom';
 
+import MainPageMain from './MainPageMain/MainPageMain';
+import MainPageNav from './MainPageNav/MainPageNav';
+import FolderPageMain from './FolderPageMain/FolderPageMain';
+import FolderPageNav from './FolderPageNav/FolderPageNav';
+
 export default class App extends React.Component {
 	render() {
 		return (
@@ -9,12 +14,16 @@ export default class App extends React.Component {
 			  	<header className='app__header'>
 					<Link to='/'><h1>Noteful</h1></Link>
 				</header>
-				<main className='app__main'>
-					
-				</main>
-				<sidebar className='app__sidebar'>
-
-				</sidebar>
+				<div class="wrapper">
+					<sidebar className='app__sidebar'>
+						<Route exact path='/' component={MainPageNav} />
+						<Route path='/folder' component={FolderPageNav} />
+					</sidebar>
+					<main className='app__main'>
+						<Route exact path='/' component={MainPageMain} />
+						<Route path='/folder' component={FolderPageMain} />
+					</main>
+				</div>
 			</div>
 		);
 	}
