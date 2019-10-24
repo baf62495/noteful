b8c97ab232baaa18b66d666546b9cb35d61c974e
baf62495 <baf62495@aol.com>
2019-10-24 17:51:44 -0400

    add folder functionality created

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import './AddFolder.css';
+import ApiContext from '../ApiContext';
+import config from '../config';
 
 export default class AddFolder extends React.Component {
+	constructor(props) {
+		super(props);
+		this.folderName = React.createRef();
+	}
+
+	static defaultProps = {
+		history: {
+			push: () => {}
+		}
+	}
+
+	static contextType = ApiContext;
+
+	handleSubmit = e => {
+		e.preventDefault();
+
+		const newFolder = {
+			name: this.folderName.current.value
+		}
+
+		fetch(`${config.API_ENDPOINT}/folders`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify(newFolder)
+		})
+			.then(res => {
+				if(!res.ok) {
+					return res.json().then(e => Promise.reject(e))
+				}
+				return res.json()
+			})
+			.then(folder => {
+				this.context.addFolder(folder)
+				this.props.history.push(`/folder/${folder.id}`)
+			})
+			.catch(err => {
+				console.error({ err })
+			})
+	}
+
 	render() {
 		return (
-			<p>This is the AddFolder component</p>
+			<section className='AddNote'>
+				<h2>Create a note</h2>
+				<form className='note__form' onSubmit={e => this.handleSubmit(e)}>
+					<div className='note__form-field'>
+						<label htmlFor='folder-name-input'>
+							Folder Name
+						</label>
+						<input
+							type='text'
+							id='folder-name-input'
+							name='folder-name'
+							ref={this.folderName}
+						/>
+					</div>
+					<button type='submit' className='btn AddFolder__btn'>
+						Add Folder
+					</button>
+				</form>
+			</section>
 		)
 	}
 }
